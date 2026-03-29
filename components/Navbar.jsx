9c7ce720ddc38ff55abdf6ad499231b73b7da738
Washington-coder <was.antonio.xy@gmail.com>
2026-03-29 15:51:31 -0400

    feat: menu for mobile devices

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -9,6 +9,7 @@ import { a } from 'framer-motion/client';
 
 const Navbar = () => {
   const [active, setActive] = useState('')
+  const [toggle, setToggle] = useState(false)
 
   return (
     <nav
@@ -35,8 +36,8 @@ const Navbar = () => {
             navLinks.map((link) => (
               <li key={link.id}
                 className={`${active === link.title
-                    ? "text-white"
-                    : "text-secondary"
+                  ? "text-white"
+                  : "text-secondary"
                   }
                   hover:text-white text-[18px] font-medium cursor-pointer
                   `}
@@ -47,6 +48,39 @@ const Navbar = () => {
             ))
           }
         </ul>
+
+        <div className='sm:hidden flex flex-1 justify-end items-center relative' >
+          <Image
+            alt='menu'
+            className='w-[28px] h-[28px] object-contain cursor-pointer'
+            src={toggle ? close : menu}
+            onClick={() => setToggle(!toggle)}
+          />
+          <div
+            className={`${!toggle ? "hidden" : "flex"} flex-col gap-4 p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+          >
+            <ul className='flex items-start flex-col gap-4'>
+              {
+                navLinks.map((link) => (
+                  <li key={link.id}
+                    className={`${active === link.title
+                      ? "text-white"
+                      : "text-secondary"
+                      }
+                    font-poppins font-medium cursor-pointer text-[16px]
+                  `}
+                    onClick={() => {
+                      setActive(link.title)
+                      setToggle(!toggle)
+                    }}
+                  >
+                    <a href={`#${link.id}`}>{link.title}</a>
+                  </li>
+                ))
+              }
+            </ul>
+          </div>
+        </div>
       </div>
     </nav>
   )
