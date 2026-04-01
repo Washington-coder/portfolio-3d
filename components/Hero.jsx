ac0d0e71363959d69efbc8a10e9e6e7a35066a26
Washington-coder <was.antonio.xy@gmail.com>
2026-04-01 14:35:46 -0400

    fix: adjust 3d model to fit into the center of the hero section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -21,7 +21,7 @@ const Hero = () => {
           <div className='w-5 h-5 rounded-full bg-[#915eff]' />
           <div className='w-1 sm:h-80 h-40 violet-gradient' />
         </div>
-        <div>
+        <div className='w-full' >
           <h1
             className={`${styles.heroHeadText} text-white `}
           >
@@ -35,7 +35,7 @@ const Hero = () => {
           </p>
           <div className='hidden sm:block w-full h-100' >
             <ComputersCanvas />
-            <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center' >
+            <div className='absolute xs:bottom-10 bottom-1 w-full flex justify-center items-center' >
               <a href='#about' >
                 <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2' >
 
