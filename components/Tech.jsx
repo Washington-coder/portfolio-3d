396b4a05b23a61777ca77d83316970ec6a2d1c9a
Washington-coder <was.antonio.xy@gmail.com>
2026-04-02 15:12:56 -0400

    feat: add tech 3d models(bals) section

--- components/Tech.jsx
+++ components/Tech.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+'use client';
+
+import { BallCanvas } from "./canvas"
+import { SectionWrapper } from "./hoc"
+import { technologies } from "@/constants"
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div className="flex flex-row flex-wrap justify-center gap-10">
+      {technologies.map((technology) => {
+        return(
+          <div key={technology.name} className="flex flex-col items-center gap-2">
+            <div className="w-28 h-28" key={technology.name} >
+              <BallCanvas icon={technology.icon.src} />
+            </div>
+            <p className="text-white text-sm font-medium">{technology.name}</p>
+          </div>
+        )
+      })}
+    </div>
   )
 }
 
-export default Tech
+export default SectionWrapper(Tech, "")
