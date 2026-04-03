9fe3a38aa70b6a848176b08479fdecddbb77c8c3
Washington-coder <was.antonio.xy@gmail.com>
2026-04-03 15:15:10 -0400

    feat: add testimonials section

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    // Se estiver no Next.js 13 ou 14, use remotePatterns (mais seguro)
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'randomuser.me',
+      },
+    ],
+    // Ou, se preferir o modo antigo/simples:
+    // domains: ['randomuser.me'],
+  },
 };
 
 export default nextConfig;
