595821b6bc6244b271c26d737158fb058a09065f
Washington-coder <was.antonio.xy@gmail.com>
2026-04-03 17:11:34 -0400

    feat: add contact form, styling, 3d model section

--- components/canvas/Earth.jsx
+++ components/canvas/Earth.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+'use client';
+
+import { Suspense } from "react"
+import { Canvas } from "@react-three/fiber"
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei"
+
+import Loader from "../Loader"
 
 const Earth = () => {
+  const earth = useGLTF('/planet/scene.gltf')
+
   return (
-    <div>Earth</div>
+    <primitive
+      object={earth.scene}
+      scale={2.5}
+    />
   )
 }
 
-export default Earth
+const EarthCanvas = () => {
+  return(
+    <Canvas
+      shadows
+      frameloop="always"
+      gl={{ preserveDrawingBuffer: true }}
+      camera={{}}
+    >
+      <Suspense fallback={<Loader />} >
+        <OrbitControls 
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+
+        <Earth />
+      </Suspense>
+    </Canvas>
+  )
+}
+
+export default EarthCanvas
